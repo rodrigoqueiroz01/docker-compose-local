206fadaa1b01b15498b51230c8ea11b0abee2622
rodrigoqueiroz01 <rodrigoq1212@gmail.com>
2023-10-22 15:04:20 -0300

    ajustes no datasource

--- postgres/datasources.sql
+++ postgres/datasources.sql
@@ -3,6 +3,28 @@
 --
 -- Name: startup; Type: DATABASE; Schema: -; Owner: startup
 --
+CREATE USER postgres WITH PASSWORD 'postgres';
+
+ALTER USER postgres WITH SUPERUSER;
+
+CREATE DATABASE postgres WITH OWNER = postgres TABLESPACE = pg_default TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'pt_BR.UTF-8' LC_CTYPE = 'pt_BR.UTF-8' CONNECTION LIMIT = -1;
+
+-- Criar schema e extensão no banco 'startup'
+\connect postgres
+
+CREATE SCHEMA public AUTHORIZATION postgres;
+
+GRANT ALL ON SCHEMA postgres TO postgres WITH GRANT OPTION;
+
+SET search_path TO postgres;
+
+ALTER DATABASE postgres SET search_path TO postgres;
+
+CREATE EXTENSION unaccent SCHEMA postgres;
+
+--
+-- Name: startup; Type: DATABASE; Schema: -; Owner: startup
+--
 CREATE USER startup WITH PASSWORD 'startup';
 
 ALTER USER startup WITH SUPERUSER;
